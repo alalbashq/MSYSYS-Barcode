dfc12cecce7bf56c868e27af3ff46cb2cab8a334
alalbashq <al.alshwmy@gmail.com>
2025-10-03 21:26:03 +0000

    enh:dd

--- mysys_barcode/public/js/item.js
+++ mysys_barcode/public/js/item.js
@@ -7,7 +7,7 @@ frappe.ui.form.on('Item', {
       }`;
       window.open(url, '_blank', 'noopener');
 
-      // لو تريده في نفس التبويب بدل السطرين أعلاه:
+      // لو تريده في نفس التبويب بدل السطرين أعلاه: 
       // frappe.set_route('quick-barcode-print');
       // window.location.hash = `#/quick-barcode-print?doc=${encodeURIComponent(frm.doctype)}/${encodeURIComponent(frm.doc.name)}`;
     }, __('Barcode'));
